22e87ba6c2c8e36a2b6266b5428892e6772f1e27
tamanegich <60025591md@gmail.com>
2025-01-13 17:17:25 +0200

    Replace for loops with filter

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -2,16 +2,7 @@
 // Return an remove without listed values
 
 Skip = (T, ...Remove) => {
-  x = 0;
-  for (C of T) {
-    for (X of Remove) {
-      if (C === X) {
-        T.splice(x, 1);
-      }
-    }
-    x++;
-  }
-  return T;
+  return T,filter(item => !Remove.includes(item))
 };
 
 module.exports = Skip;
