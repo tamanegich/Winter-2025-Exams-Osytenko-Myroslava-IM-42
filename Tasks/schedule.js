3587a921fcb3331b6f1f3e7704d28b67000a7b08
tamanegich <60025591md@gmail.com>
2025-01-13 16:20:55 +0200

    Add early return for invalid n value

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const schedule = async (func, n, interval) => {
+  if (n <= 0) return;
   for (let index = 0; index < n; index++) {
     await new Promise(resolve => {
       setTimeout(() => {
